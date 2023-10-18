fb857425606aa3141460acb8525c838b12f8d45d
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2023-10-18 12:05:03 +0800

    Added helloworld compnent

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <p class="text-3xl text-red-600">HELLO</p>
   <!-- Anything that is in public will be in root folder -->
   <img src="/ThreadsIcon.svg" alt="Threads logo" /> 
+  <HelloWorld></HelloWorld>
 
 
 </template>
