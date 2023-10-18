b14efb8a45ff15e835152af06942a299b66906cc
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2023-10-18 13:50:27 +0800

    Added new things to change

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,11 +4,21 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
 
+<div>
   <HelloWorld msg="Vite + Vue" />
   <p class="text-3xl text-red-600">HELLO</p>
   <!-- Anything that is in public will be in root folder -->
-  <img src="/ThreadsIcon.svg" alt="Threads logo" /> 
-  <HelloWorld></HelloWorld>
+  <img src="/ThreadsIcon.svg" alt="Threads logo" class="animate__animated animate__fadeIn from-right"/> 
 
+  <!-- <HelloWorld></HelloWorld> -->
+  <div class="animate-pulse flex space-x-4">
+    <p>hello</p>
+  </div>
+
+  <div class="animate-bounce w-6 h-6 ...">
+    <p>byebeybey</p>
+    <p>hello</p>
+  </div>
+</div>
 
 </template>
