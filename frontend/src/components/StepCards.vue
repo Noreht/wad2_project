c7eeabb395cb10e310ee3f9584294186565b08fa
CaiJun01 <105629620+CaiJun01@users.noreply.github.com>
2023-10-23 00:58:50 +0800

    basiccompost page

--- frontend/src/components/StepCards.vue
+++ frontend/src/components/StepCards.vue
@@ -1,19 +1,16 @@
 
 <template>
-    <div class=" mt-3 bg-[rgb(255,255,255,0.3)] rounded-2xl border-2 border-amber-700">
+    <div class=" my-10 py-5 bg-[rgb(255,255,255,0.3)] rounded-2xl border-2 border-amber-700">
       <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="md:grid md:grid-cols-4 mx-auto lg:mx-0">
-            <h2 class="md:col-span-3 text-2xl text-black py-5 leading-10 font-semibold tracking-normal">{{ header }}</h2>
-            <div
-                class="mb-4 border-black rounded-t-xl md:col-span-1 md:row-span-2"
-                :style="{
-                backgroundImage: `url(${image})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'contain',
-                backgroundPosition: 'top center',
-                }"
-            ></div>
-            <p class=" md:col-span-3 pb-5 text-sm leading-5 font-semibold text-black">{{ body }}</p>
+        <div class="min-[1030px]:grid grid-cols-4 mx-auto lg:mx-0">
+            <h2 class="col-span-3 text-2xl sm:text-3xl text-black py-5 leading-10 font-semibold tracking-normal">{{ header }}</h2>
+            
+            <p class=" order-last col-span-3 pb-5 sm:text-xl leading-5 font-semibold text-black">{{ body }}</p>
+            <div 
+                class=" flex justify-center max-h-60 mb-4 border-black rounded-t-xl col-span-1 row-span-2 place-self-center"
+                >
+                <img class="max-h-60" :src='image'>
+            </div>
             
 
           
@@ -30,7 +27,7 @@
 <script>
 export default {
     props: {
-      header: String,    // Features list
+      header: String, 
       body: String,
       image: String
 
