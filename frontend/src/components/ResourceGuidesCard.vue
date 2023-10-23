ce06da33ce1925bb6c0f982943123559021af21b
JermYong <jeremyyong13@gmail.com>
2023-10-23 15:36:41 +0800

    ok not bad

--- frontend/src/components/ResourceGuidesCard.vue
+++ frontend/src/components/ResourceGuidesCard.vue
@@ -1,42 +1,39 @@
-<template >
-    <div class=" me-3 w-[70%] max-w-[200px] min-w-[140px] flex-none rounded-2xl border-4 border-green-900">
-
+<template>
+  <a
+    :href="url1"
+    class="me-3 w-[70%] max-w-[200px] min-w-[140px] flex-none rounded-2xl border-4 border-green-900"
+  >
     <!-- <img class="rounded-2" src={{image}} alt="Sunset in the mountains"> -->
     <div
-        class=" h-[150px] border-green-900 rounded-t-xl border-b-4"
-        :style="{
-          backgroundImage: `url(${image})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'top center',
-        }"
-      ></div>
-        
+      class="h-[150px] border-green-900 rounded-t-xl border-b-4"
+      :style="{
+        backgroundImage: `url(${image})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'top center',
+      }"
+    ></div>
+
     <div class="px-3 py-2 bg-[rgb(0,0,0,)] rounded-b-xl">
-        <div class="font-bold text-sm mb-2">How to:</div>
-        <p class="text-gray-700 text-base">
-            {{title}}
-        </p>
+      <div class="font-bold text-sm mb-2">How to:</div>
+      <p class="text-gray-700 text-base">
+        {{ title }}
+      </p>
     </div>
     <!-- <div class="px-6 pt-4 pb-2">
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
         <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
     </div> -->
-    </div>
+  </a>
 </template>
-
-
-
 
 <script>
 export default {
-    props: {
-      title: String,    // title name
-      image: String,    // image URL
-
-    }
-  };
-
-
+  props: {
+    title: String, // title name
+    image: String, // image URL
+    url1: String,
+  },
+};
 </script>
--- frontend/src/views/ResourceGuidesView.vue
+++ frontend/src/views/ResourceGuidesView.vue
@@ -3,8 +3,10 @@
     <div class="col-span-2 p-4"></div>
     <div class="col-span-8 p-4 place-content-center">
       <!-- ADD THE COMPONENTS AND STYLING HERE -->
-      <h1 class="text-center text-5xl sm:text-6xl font-semibold">Resources and Guides</h1>
-      <h3 class=" py-4 text-center text-lg sm:text-xl font-semibold leading-10">
+      <h1 class="text-center text-5xl sm:text-6xl font-semibold">
+        Resources and Guides
+      </h1>
+      <h3 class="py-4 text-center text-lg sm:text-xl font-semibold leading-10">
         Explore and kick start your journey today!
       </h3>
       <ResourceGuidesSummary
@@ -14,47 +16,57 @@
       <div id="beginner" class="">
         <h3 class="text-2xl font-semibold mt-8">Starter's Haven</h3>
         <h3 class="text-lg font-normal mb-3">For precious beginners</h3>
-        <div class="border-4 border-black rounded-3xl p-4 bg-green-300 overflow-x-auto overflow-hidden flex first:pl-6 last:pr-6">
+        <div
+          class="border-4 border-black rounded-3xl p-4 bg-green-300 overflow-x-auto overflow-hidden flex first:pl-6 last:pr-6"
+        >
           <ResourceGuidesCard
             :title="'Make a Basic Compost?'"
             :image="'ResourceGuidesCards/compostbin1.png'"
+            :url1="'/basiccompost'"
           />
           <ResourceGuidesCard
             :title="'Build a Composting Bin?'"
             :image="'ResourceGuidesCards/compostbin.png'"
+            :url1="''"
           />
           <ResourceGuidesCard
             :title="'Keep your Compost Warm?'"
             :image="'ResourceGuidesCards/warm.png'"
+            :url1="''"
           />
           <ResourceGuidesCard
             :title="'Speed up Composting Process?'"
             :image="'ResourceGuidesCards/time.png'"
+            :url1="''"
           />
           <ResourceGuidesCard
             :title="'Keep your Compost from Smelling?'"
             :image="'ResourceGuidesCards/odor.png'"
+            :url1="''"
           />
           <ResourceGuidesCard
             :title="'Process Food Waste before using?'"
             :image="'ResourceGuidesCards/food.png'"
+            :url1="''"
           />
         </div>
       </div>
 
       <div id="intermediate" class="">
-          <h3 class="text-2xl font-semibold mt-8">Next-Level Navigators</h3>
-          <h3 class="text-lg font-normal mb-3">For experienced composters</h3>
-          <div class="border-4 p-4 rounded-3xl border-black bg-green-400 overflow-x-auto overflow-hidden flex">
-            <ResourceGuidesCard :title="'Build a Tumbling Composter?'" />
-            <ResourceGuidesCard :title="'Apply Mulch to Compost?'" />
-            <ResourceGuidesCard :title="'Make a Worm Compost System?'" />
-            <ResourceGuidesCard :title="'Upscale your Compost Bin'" />
-          </div>
+        <h3 class="text-2xl font-semibold mt-8">Next-Level Navigators</h3>
+        <h3 class="text-lg font-normal mb-3">For experienced composters</h3>
+        <div
+          class="border-4 p-4 rounded-3xl border-black bg-green-400 overflow-x-auto overflow-hidden flex"
+        >
+          <ResourceGuidesCard :title="'Build a Tumbling Composter?'" />
+          <ResourceGuidesCard :title="'Apply Mulch to Compost?'" />
+          <ResourceGuidesCard :title="'Make a Worm Compost System?'" />
+          <ResourceGuidesCard :title="'Upscale your Compost Bin'" />
+        </div>
       </div>
     </div>
-    </div>
-    <div class="col-span-2 px-4"></div>
+  </div>
+  <div class="col-span-2 px-4"></div>
 
   <Foot />
 </template>
