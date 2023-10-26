8eede3ff5dca1f5b92c57b24e2c1d012c09b3152
JermYong <jeremyyong13@gmail.com>
2023-10-27 02:38:09 +0800

    accordion sui sui

--- frontend/src/views/HomepageView.vue
+++ frontend/src/views/HomepageView.vue
@@ -86,7 +86,7 @@
         </h1>
       </div>
 
-      <div class="lg:flex gap-5 lg:content-stretch">
+      <div class="lg:flex lg:content-stretch gap-5 justify-center">
         <ImpactCard
           :feature="'Individual'"
           :content="'I’m just looking to try out composting within my household and learning more about the movement!'"
@@ -141,86 +141,279 @@
       <div>
         <TwitterFeed />
       </div>
-      <div class="grid place-items-center ">
-        <div class="w-6/12 mx-auto rounded-xl border min-w-[450PX]">
-          <div class="bg-yellow-900 p-10 shadow-sm rounded-xl -m-[10px]">
-            <!-- ... your other content ... -->
-            <h1
-              class="text-2xl font-bold tracking-tight text-white sm:text-3xl"
+      <div class="grid place-items-center">
+        <div
+          class="py-5 px-10 rounded-xl -m-[20px] border-black border-[3px] bg-white"
+          :style="{
+            background:
+              'linear-gradient(to bottom, white 0%, white 22.5%, rgb(99 49 18) 20%, rgb(99 49 18) 100%)',
+          }"
+        >
+          <h1 class="font-extrabold tracking-tight text-green-900 text-4xl">
+            FAQs
+          </h1>
+          <!-- Accordion Wrapper -->
+          <div
+            class="transition hover:bg-amber-300 bg-gray-500 rounded-xl text-white mt-2 hover:text-green-900"
+          >
+            <!-- header -->
+            <div
+              class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
+              @click="toggleAccordion('vision')"
             >
-              FAQ
-            </h1>
-            <!-- Accordion Wrapper -->
-            <div class="transition hover:bg-amber-500 bg-black rounded-xl text-white mt-2" >
-              <!-- header -->
-              <div
-                class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
-                @click="toggleAccordion('vision')"
-              >
-                <i
-                  class="fas"
-                  :class="{
-                    'fa-plus': !accordionStatus.vision,
-                    'fa-minus': accordionStatus.vision,
-                  }"
-                ></i>
-                <h3 class="font-bold">What is the long-term vision for BlackGold?</h3>
-              </div>
-              <!-- Content -->
-              <div
-                class="accordion-content px-5 pt-0 overflow-hidden"
-                :style="{
-                  maxHeight: accordionStatus.vision ? 'none' : '0',
-                }"
-              >
-                <p class="leading-6 mb-4 p-2 text-justify bg-amber-500 rounded-2xl text-black font-medium">
-                  At BlackGold, our mission is driven by the commitment to
-                  support Singapore's ambitious 30 by 30 food security plan. Our
-                  goal is to cultivate a productive, resilient, and
-                  resource-efficient agri-food industry.
-                  <button
-                  class="rounded-full bg-indigo-600 text-white font-medium font-lg px-6 py-2 mt-3 "
+              <span class="flex items-center"
+                ><svg
+                  class="w-5 h-5 shrink-0"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  Learn more
-                </button>
-                </p>
-                
-              </div>
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+
+              <h3 class="font-bold sm:text-xl">
+                What is the long-term vision for BlackGold?
+              </h3>
             </div>
-            <div class="transition hover:bg-amber-500 bg-black rounded-xl text-white mt-2" >
-              <!-- header -->
-              <div
-                class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
-                @click="toggleAccordion('vision')"
+            <!-- Content -->
+            <div
+              class="accordion-content px-5 pt-0 overflow-hidden"
+              :style="{
+                maxHeight: accordionStatus.vision ? 'none' : '0',
+              }"
+            >
+              <p
+                class="leading-6 mb-4 p-2 bg-white rounded-2xl text-black font-medium"
               >
-                <i
-                  class="fas"
-                  :class="{
-                    'fa-plus': !accordionStatus.vision,
-                    'fa-minus': accordionStatus.vision,
-                  }"
-                ></i>
-                <h3 class="font-bold">What is the long-term vision for BlackGold?</h3>
-              </div>
-              <!-- Content -->
-              <div
-                class="accordion-content px-5 pt-0 overflow-hidden"
-                :style="{
-                  maxHeight: accordionStatus.vision ? 'none' : '0',
-                }"
-              >
-                <p class="leading-6 mb-4 p-2 text-justify bg-amber-500 rounded-2xl text-black font-medium">
-                  At BlackGold, our mission is driven by the commitment to
-                  support Singapore's ambitious 30 by 30 food security plan. Our
-                  goal is to cultivate a productive, resilient, and
-                  resource-efficient agri-food industry.
-                  <button
-                  class="rounded-full bg-indigo-600 text-white font-medium font-lg px-6 py-2 mt-3 "
+                At BlackGold, our mission is driven by the commitment to support
+                Singapore's ambitious 30 by 30 food security plan. Our goal is
+                to cultivate a productive, resilient, and resource-efficient
+                agri-food industry. <br />
+                <button
+                  class="rounded-full bg-green-900 hover:bg-green-600 text-white font-medium font-lg px-6 py-2 mt-3"
                 >
                   Learn more
                 </button>
-                </p>
-                
+              </p>
+            </div>
+          </div>
+          <div
+            class="transition hover:bg-amber-300 bg-gray-500 rounded-xl text-white mt-2 hover:text-green-900"
+          >
+            <!-- header -->
+            <div
+              class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
+              @click="toggleAccordion('vision1')"
+            >
+              <span class="flex items-center"
+                ><svg
+                  class="w-5 h-5 shrink-0"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+
+              <h3 class="font-bold sm:text-xl">
+                Who are our community partners?
+              </h3>
+            </div>
+            <!-- Content -->
+            <div
+              class="accordion-content px-5 pt-0 overflow-hidden"
+              :style="{
+                maxHeight: accordionStatus.vision1 ? 'none' : '0',
+              }"
+            >
+              <p
+                class="leading-6 mb-4 p-2 bg-white rounded-2xl text-black font-medium"
+              >
+                BlackGold collaborates with various community partners,
+                including local environmental organizations, schools, and
+                neighborhood associations. These partners help us organize
+                workshops, awareness campaigns, and community composting
+                programs. By working together, we can expand the reach and
+                impact of the initiative. <br />
+                <button
+                  class="rounded-full bg-green-900 hover:bg-green-600 text-white font-medium font-lg px-6 py-2 mt-3"
+                >
+                  Learn more
+                </button>
+              </p>
+            </div>
+          </div>
+          <div
+            class="transition hover:bg-amber-300 bg-gray-500 rounded-xl text-white mt-2 hover:text-green-900"
+          >
+            <!-- header -->
+            <div
+              class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
+              @click="toggleAccordion('vision2')"
+            >
+              <span class="flex items-center"
+                ><svg
+                  class="w-5 h-5 shrink-0"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+
+              <h3 class="font-bold sm:text-xl">
+                Is BlackGold open to businesses and organizations?
+              </h3>
+            </div>
+            <!-- Content -->
+            <div
+              class="accordion-content px-5 pt-0 overflow-hidden"
+              :style="{
+                maxHeight: accordionStatus.vision2 ? 'none' : '0',
+              }"
+            >
+              <p
+                class="leading-6 mb-4 p-2 bg-white rounded-2xl text-black font-medium"
+              >
+                Yes, BlackGold is open to businesses and organizations
+                interested in sustainable waste management practices. We
+                encourage local businesses, schools, and institutions to
+                participate in our corporate composting programs. By doing so,
+                they can reduce their environmental footprint and contribute to
+                a greener, more sustainable Singapore. <br />
+                <button
+                  class="rounded-full bg-green-900 hover:bg-green-600 text-white font-medium font-lg px-6 py-2 mt-3"
+                >
+                  Learn more
+                </button>
+              </p>
+            </div>
+          </div>
+          <div
+            class="transition hover:bg-amber-300 bg-gray-500 rounded-xl text-white mt-2 hover:text-green-900"
+          >
+            <!-- header -->
+            <div
+              class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
+              @click="toggleAccordion('vision3')"
+            >
+              <span class="flex items-center"
+                ><svg
+                  class="w-5 h-5 shrink-0"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+
+              <h3 class="font-bold sm:text-xl">
+                Is BlackGold a government-funded project?
+              </h3>
+            </div>
+            <!-- Content -->
+            <div
+              class="accordion-content px-5 pt-0 overflow-hidden"
+              :style="{
+                maxHeight: accordionStatus.vision3 ? 'none' : '0',
+              }"
+            >
+              <p
+                class="leading-6 mb-4 p-2 bg-white rounded-2xl text-black font-medium"
+              >
+                BlackGold is supported by government initiatives and funding in
+                partnership with the National Environment Agency (NEA) of
+                Singapore. While it receives governmental support, it is also
+                dependent on community involvement and contributions from
+                participants and sponsors. <br />
+                <button
+                  class="rounded-full bg-green-900 hover:bg-green-600 text-white font-medium font-lg px-6 py-2 mt-3"
+                >
+                  Learn more
+                </button>
+              </p>
+            </div>
+          </div>
+          <div
+            class="transition hover:bg-amber-300 bg-gray-500 rounded-xl text-white mt-2 hover:text-green-900"
+          >
+            <!-- header -->
+            <div
+              class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16"
+              @click="toggleAccordion('vision4')"
+            >
+              <span class="flex items-center"
+                ><svg
+                  class="w-5 h-5 shrink-0"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+
+              <h3 class="font-bold sm:text-xl">
+                What support does BlackGold offer to participants?
+              </h3>
+            </div>
+            <!-- Content -->
+            <div
+              class="accordion-content px-5 pt-0 overflow-hidden"
+              :style="{
+                maxHeight: accordionStatus.vision4 ? 'none' : '0',
+              }"
+            >
+              <div
+                class="leading-6 mb-4 p-2 bg-white rounded-2xl text-black font-medium"
+              >
+                BlackGold offers a range of support to participants, including:
+                <li>
+                  Educational resources on composting techniques and best
+                  practices.
+                </li>
+                <li>
+                  Access to composting bins and materials at discounted rates.
+                </li>
+                <li>
+                  Workshops, seminars, and community events to enhance
+                  composting knowledge.
+                </li>
+                <li>Assistance in setting up community composting sites.</li>
+                <li>
+                  Regular updates on the impact of your composting efforts and
+                  how they contribute to a greener, more sustainable Singapore.
+                </li>
+
+                <button
+                  class="rounded-full bg-green-900 hover:bg-green-600 text-white font-medium font-lg px-6 py-2 mt-3"
+                >
+                  Learn more
+                </button>
               </div>
             </div>
           </div>
@@ -228,7 +421,9 @@
       </div>
     </div>
 
-    <div class="col-span-2 p-4"></div>
+    <div
+      class="col-span-2 p-4"
+    ></div>
   </div>
   <Foot />
 </template>
@@ -260,15 +455,22 @@ export default {
       isGoldVisible: false,
       loopTyping: true,
       accordionStatus: {
-        whatIsTerm: false,
-        whenToUseAccordion: false,
-        howCanItBeDefined: false,
-        chamberReached: false,
+        vision: false,
+        vision1: false,
+        vision2: false,
+        vision3: false,
+        vision4: false,
       },
+
     };
   },
   methods: {
     toggleAccordion(accordionName) {
+      for (const key in this.accordionStatus) {
+      if (key !== accordionName) {
+        this.accordionStatus[key] = false;
+      }
+      }
       this.accordionStatus[accordionName] =
         !this.accordionStatus[accordionName];
     },
