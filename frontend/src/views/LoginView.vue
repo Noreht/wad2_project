4ed74dc2d411eaa2605381431b24df749dcef171
Bryan Tan Jia Jun <bryan.tanjiajun@gmail.com>
2023-10-27 15:04:17 +0800

    debugged LoginImageCard component  using swiper

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -8,46 +8,50 @@
     </div>
 
     <div class="flex-col justify-center xl:px-24 hidden xl:block">
-    <div id="default-carousel" class="relative w-full" data-carousel="slide">
-      <div class="relative overflow-hidden rounded-lg xl:py-72">
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <LoginImageCard
-            :feature="'Nature\'s Recycler'"
-            :content="'Composting: Reviving the Earth, One Pile at a Time'"
-            :image="'LoginImageCards/HandsCompost.svg'"
-            :hoverImage="'LoginImageCards/HandsCompost.jpg'"
-          />
-        </div>
-
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <LoginImageCard
+      <swiper-container
+        :slides-per-view="1"
+        :space-between="spaceBetween"
+        :centered-slides="true"
+        @swiperprogress="onProgress"
+        @swiperslidechange="onSlideChange"
+        :autoplay="{ delay: 2500 }"
+        :loop="true"
+      >
+    <swiper-slide>          
+      <LoginImageCard
+        :feature="'Nature\'s Recycler'"
+        :content="'Composting: Reviving the Earth, One Pile at a Time'"
+        :image="'LoginImageCards/HandsCompost.svg'"
+        :hoverImage="'LoginImageCards/HandsCompost.jpg'"
+      />
+    </swiper-slide>
+    <swiper-slide>
+      <LoginImageCard
             :feature="'Compost Magic'"
             :content="'Turning Kitchen Scraps into Garden Gold'"
             :image="'LoginImageCards/CompostBinWaste.svg'"
             :hoverImage="'ImpactCards/Contributor.jpg'"
           />
-        </div>
-
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <LoginImageCard
+    </swiper-slide>
+    <swiper-slide>
+      <LoginImageCard
             :feature="'Waste to Wealth'"
             :content="'Where Waste Becomes Wonder'"
             :image="'LoginImageCards/GardenFruits.svg'"
             :hoverImage="'LoginImageCards/GardenFruits.svg'"
           />
-        </div>
-
-        <!-- Reset transition properties for the first slide -->
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-          <LoginImageCard
+    </swiper-slide>
+      <swiper-slide>
+        <LoginImageCard
             :feature="'Get Dirty'"
             :content="'Composting Feeds the World'"
             :image="'LoginImageCards/GardeningSolarPunk.svg'"
             :hoverImage="'LoginImageCards/GardeningSolarPunk.svg'"
           />
-        </div>
-      </div>
-    </div>
+      </swiper-slide>
+
+  </swiper-container>
+      
 </div>
 
 
@@ -62,8 +66,33 @@
   import LoginCard from "@/components/LoginCard.vue";
   import LoginImageCard from "@/components/LoginImageCard.vue";
   import 'flowbite/dist/flowbite.min.js';
+  import { register } from 'swiper/element/bundle';
+
+  register();
+
   export default {
     name: "LoginView",
+    
+    setup() {
+      const spaceBetween = 10;
+      const onProgress = (e) => {
+        const [swiper, progress] = e.detail;
+        console.log(progress)
+      };
+
+      const onSlideChange = (e) => {
+        console.log('slide changed')
+      }
+
+      return {
+        spaceBetween,
+        onProgress,
+        onSlideChange,
+      }
+
+    },
+
+
     components: {
       LoginCard,
       LoginImageCard
