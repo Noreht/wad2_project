4a87aca5c9351deeda4247e9fba16f4a67c9603b
maht-noreht <139751248+maht-noreht@users.noreply.github.com>
2023-11-05 01:09:28 +0800

    Updated eventcarousel to be nicer
    Updated event carousel to be nicer

--- frontend/src/components/EventCarousel.vue
+++ frontend/src/components/EventCarousel.vue
@@ -21,38 +21,44 @@
         :style="innerStyles"
       >
         <div
-          class="jumbo absolute w-11/12"
+          class="jumbo absolute w-11/12 pl-10"
           v-for="(jumbo, index) in eventList"
-          :key="jumbo"
+          :key="index"
         >
           <div class="w-full pl-4 pb-1 h-72">
             <div
-              class="h-32 sm:h-64 sm:p-2  bg-amber-400 rounded-xl shadow-lg dark:bg-gray-800 grid grid-cols-2 relative p-2 "
+              class="h-32 sm:h-64 sm:p-2  bg-gradient-to-r from-black to-amber-400  rounded-xl shadow-lg dark:bg-gray-800 grid grid-cols-2 relative p-2 "
             >
               <div class="overflow-y-auto">
                 <h1
-                  class="text-base font-extrabold tracking-tight leading-none text-gray-900 sm:text-3xl lg:text-4xl dark:text-white underline mb-2 break-normal whitespace-normal"
+                  class="drop-shadow-xl font-bold tracking-tight text-white sm:text-3xl lg:text-4xl dark:text-white underline mb-2 break-normal whitespace-normal pb-2 pl-2"
                 >
                   {{ jumbo.EventName }}
                 </h1>
 
                 <h3
-                  class="text-base mb-1 font-bold tracking-tight leading-none text-gray-900 sm:text-2xl lg:text-3xl dark:text-white italic"
+                  class="text-base mb-1 font-bold tracking-tight leading-none text-white sm:text-2xl lg:text-3xl dark:text-white italic pb-2 pl-2"
                 >
                   {{ jumbo.EventDate }}
                 </h3>
 
                 <h4
-                  class="mb-1 text-sm font-bold tracking-tight leading-none text-gray-900 sm:text-2xl lg:text-3xl dark:text-white italic"
+                  class="mb-1 text-sm font-bold tracking-tight leading-none text-white sm:text-2xl lg:text-3xl dark:text-white italic pb-2 pl-2"
                 >
                   {{ jumbo.EventOrganiser }}
                 </h4>
 
-                <a
+                <p
+                  class="mb-1 pr-2 text-lg font-normal text-white sm:text-xl dark:text-gray-400 break-normal whitespace-normal pl-2"
+                >
+                  {{ jumbo.EventDescription }}
+                </p>
+
+                <!-- <a
                   href="#"
                   class="text-blue-600 dark:text-blue-500 hover:underline font-medium text-xs inline-flex items-center md:xl"
                   >Learn more
-                  <svg
+                <svg
                     class="w-3.5 h-3.5 ml-1"
                     aria-hidden="true"
                     xmlns="{{eventPage}}"
@@ -65,16 +71,12 @@
                       stroke-linejoin="round"
                       stroke-width="2"
                       d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
-                  </svg>
-                </a>
+                    /> -->
+                  <!-- </svg>
+                </a> -->
               </div>
-              <div class="overflow-y-auto">
-                <p
-                  class="mb-1 text-sm font-normal text-black sm:text-xl dark:text-gray-400 break-normal whitespace-normal"
-                >
-                  {{ jumbo.EventDescription }}
-                </p>
+              <div class="overflow-y-auto grid justify-items-center align-middle">
+                <img :src="images[i]" class=" pl-2 bg-blend-lighten shadow rounded max-w-full h-full flex align-middle border-none" >
               </div>
               <button
                 class="absolute bottom-5 right-12 w-8 h-8 rounded-full bg-black hover:bg-gray-500 text-white"
@@ -146,29 +148,11 @@ export default {
       typewriterText: "",
       isGoldVisible: false,
       loopTyping: true,
-      EventJumbotron: [
-        {
-          Header: "Head 1",
-          Organiser: "Org 1",
-          Date: "11 Oct, 12pm",
-          Desc: "lorem ispum dolor",
-        },
-        {
-          Header: "Head 2",
-          Organiser: "Org 2",
-          Date: "11 Oct, 12pm",
-          Desc: "lorem ispum dolor 2",
-        },
-        {
-          Header: "Head 3",
-          Organiser: "Org 3",
-          Date: "11 Oct, 12pm",
-          Desc: "lorem ispum dolor 3",
-        },
-      ],
       innerStyles: {},
       step: "",
       transitioning: false,
+      i:0,
+      images: ["https://as2.ftcdn.net/v2/jpg/04/16/88/91/1000_F_416889138_FGxmM0mXGlcdw12N2mKeKmiK70NyM6rS.jpg", "https://as1.ftcdn.net/v2/jpg/03/71/76/22/1000_F_371762244_Yw95Ozaiy5Bn4gDklWspAt4RzRHdhQr5.jpg", "https://t3.ftcdn.net/jpg/02/01/20/46/240_F_201204650_4XuG7U4GulgYMB5jEEH18DLaBmoTlFzf.jpg", "https://t3.ftcdn.net/jpg/02/82/11/12/240_F_282111267_0dl5FSUofp0tslSgZdVwF9ldiBfBFJl6.jpg", "https://t3.ftcdn.net/jpg/05/77/80/74/240_F_577807499_ifzuGHCTGspZWVq7jp4Dj1y4wMWalJKY.jpg", "https://t4.ftcdn.net/jpg/06/00/67/75/240_F_600677520_N4fpaLL43yr2LSjvztcW5CFXV9hOLPeb.jpg", "https://t4.ftcdn.net/jpg/05/69/34/09/240_F_569340944_6rb7vtDV65TvWiMcRYcOleqNrIbQbchC.jpg", "https://t4.ftcdn.net/jpg/02/45/24/59/240_F_245245911_kAXFCW93xISiKU42JmxWx41sRTb6tauT.jpg"]
     };
   },
 
@@ -242,6 +226,14 @@ export default {
         this.resetTranslate();
         this.transitioning = false;
       });
+
+      if (this.i === this.eventList.length - 1) {
+        return this.i -= 1;
+      }
+
+      return this.i += 1;
+
+      
     },
 
     prev() {
@@ -257,6 +249,12 @@ export default {
         this.resetTranslate();
         this.transitioning = false;
       });
+
+      if (this.i == 0) {
+        return this.i += 1;
+      }
+
+      return this.i -= 1;
     },
 
     moveLeft() {
